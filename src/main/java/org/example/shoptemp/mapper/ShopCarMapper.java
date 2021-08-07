1c174d9d3a54d079ac62302c677a17ff56f88de6
chenzhixiang <313102983@qq.com>
2021-08-07 15:03:57 +0800

    购物车

--- src/main/java/org/example/shoptemp/mapper/ShopCarMapper.java
+++ src/main/java/org/example/shoptemp/mapper/ShopCarMapper.java
@@ -1,6 +1,7 @@
 package org.example.shoptemp.mapper;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Param;
 import org.example.shoptemp.entity.ShopCar;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.example.shoptemp.vo.ShopCarVO;
@@ -15,7 +16,7 @@ public interface ShopCarMapper extends BaseMapper<ShopCar> {
      * @param buyerId
      * @return
      */
-    Page<ShopCarVO> selectByPage(Page<ShopCar> page, Integer buyerId);
+    Page<ShopCarVO> selectByPage(@Param("page") Page<ShopCar> page,@Param("buyerId") Integer buyerId);
 }
 
 
